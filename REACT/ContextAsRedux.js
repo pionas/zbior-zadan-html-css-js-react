9a5b71e704d1926ceb15601ce3f8c61ff477a7df
Pionas <adrian@pionka.com>
2020-11-05 20:59:11 +0100

    Update ContextAsRedux.js

--- REACT/ContextAsRedux.js
+++ REACT/ContextAsRedux.js
@@ -1,3 +1,4 @@
+import React, { useContext, useReducer } from "react";
 // Logika Reduxa w React składa się z 5 prostych elementów:
 // - struktury danych - tzn store/state - komórka pamięci z aktualnym stanem wszystkich danych
 // - reducera, który agreguje akcje i zmienia wg nich aktualny stan store
@@ -13,48 +14,106 @@
 
 // na podstawie poniższego kodu
 // odtwórz Reduxa w React ContextApi
+
 const initialState = {
     text:'treść'
 }
 
+//REACT CONTEXT - 'STORE'
 const ctx = React.createContext({
     state: initialState,
-    actions:{
-        changeText:(state, action)=>{}
-    }
+    actions: actions,
 })
 
+// ACTION TYPE
+const TEXT_CHANGED= 'TEXT_CHANGED';
+
+//ACTION CREATED
 const actions = {
-    changeText:(state, action)=>{
-        // return ...
+    changeText:(text)=>({
+        type: TEXT_CHANGED,
+        payload: text,
+    })
+}
+
+//REDUCER
+function reducer(state = initialState, action){
+    switch (action.type) {
+        case TEXT_CHANGED:
+            return {
+                ...state,
+                text: action.payload
+            }
+        default:
+            return state;
     }
 }
 
-function reducer(state=initialState, action){
-    // zwykły reducer
-    // return ...
-}
-
+// PROVIDER
 const Provider = ({children, onLoad, onChange}) => {
+    const context = useContext(ctx);
     // tutaj łączymy context i actions i wrzucamy je do providera
     // prop onLoad powinien wywołać się na wczytaniu komponentu
     // prop onChange powinien wywołać się na zmianie stanu
     // return ...
+
+    const contextValue = useReducer(reducer, initialState);
+    
+    return (
+        <context.Provider value={contextValue} onLoad={onLoad} onChange={onChange}>
+            {children}
+        </context.Provider>
+    )
 }
 
-const useContextState = ({stateNames=['text']})=>{
+// HOOKS
+const useContextState = ({stateNames=['text']}) => {
     // jeśli stateNames jest pusty to zwraca cały state
     // jeśli stateNames nie jest pusty to zwraca podane w arrayu klucze i wartości w formie nowego obiektu
     // return ...
+    const hasNoArguments = stateNames.length === 0;
+    const { state } = useContext(ctx);
+
+    if (hasNoArguments || !stateNames) { return state }
+
+    const allStates = {...state};
+
+    return stateNames.reduce((filteredState, key) => {
+        if (typeof allStates[key] !== 'undefined') {
+            filteredState[key] = allStates[key];
+        }
+        return filteredState;
+    }, {};
 }
 
 const useContextActions = ({actions=["changeText"]})=>{
     // jeśli actions jest pusty to zwraca wszystkie akcje
     // jeśli actions nie jest pusty to zwraca akcje wskazane po nazwie w arryu actions
     // return ...
+    const hasNoArguments = actions.length === 0;
+    const { actions } = useContext(ctx);
+
+    if (hasNoArguments || !actions) { return actions }
+
+    const allActions = {...actions};
+
+    return actions.reduce((filteredState, key) => {
+        if (typeof allActions[key] !== 'undefined') {
+            filteredState[key] = allActions[key];
+        }
+        return filteredState;
+    }, {};
 }
 
 const useContextActionsAndStore = ({actions=["changeText"], stateNames=['text']})=>{
     // suma logiki powyżej
     // return ...
+    const queriedActions = useContextActions(actions); 
+    const queriedStates = useContextState(stateNames);
+    const queriedStatesAndActions = {...queriedActions, ...queriedStates}
+    return queriedStatesAndActions;
 }
+
+// CONSUMER 
+
+const ContextConsumer = ctx.Consumer;
